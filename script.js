1a2943eac113ca7744b7390a5169b6b82574b1ff
Harivel2021 <harivel504@gmail.com>
2024-07-10 22:38:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function calculateBMI() {
+    // Get input values
+    var height = parseFloat(document.getElementById('height').value);
+    var weight = parseFloat(document.getElementById('weight').value);
+  
+    // Calculate BMI
+    var bmi = weight / ((height / 100) ** 2);
+  
+    // Display result
+    var resultElement = document.getElementById('result');
+    resultElement.innerHTML = `Your BMI is ${bmi.toFixed(2)}`;
+  
+    // Add BMI category based on WHO guidelines
+    var category;
+    if (bmi < 18.5) {
+      category = 'TAKE FOOD';
+    } else if (bmi >= 18.5 && bmi < 25) {
+      category = 'MAINTAIN IT';
+    } else if (bmi >= 25 && bmi < 30) {
+      category = 'NEED TO TAKE DIET FOOD';
+    } else {
+      category = 'AVOID TAKING JUNK FOOD';
+    }
+  
+    resultElement.innerHTML += `<br><strong>Category:</strong> ${category}`;
+  }
+  
+
